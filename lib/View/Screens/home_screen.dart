20d105b07f01143e774742f02efe1b4cae0eb66d
Karan Gupta <gupta.karan1162@gmail.com>
2024-07-24 10:29:31 +0530

    .

--- lib/View/Screens/home_screen.dart
+++ lib/View/Screens/home_screen.dart
@@ -10,6 +10,12 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return const Scaffold(
+      body: Center(
+        child: Text(
+          'Home screen',
+        ),
+      ),
+    );
   }
 }
