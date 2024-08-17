31ec7e03ee7156d2bbafd19e3323061830e61b2b
Karan Gupta <gupta.karan1162@gmail.com>
2024-08-17 13:42:42 +0530

    .

--- lib/View/Screens/home_screen.dart
+++ lib/View/Screens/home_screen.dart
@@ -27,7 +27,7 @@ class _HomeScreenState extends State<HomeScreen> {
                   'This is the first screen',
                   style: TextStyle(
                     fontSize: 50,
-                    fontWeight: FontWeight.bold,
+                    fontWeight: FontWeight.bold
                   ),
                 ),
               ],
